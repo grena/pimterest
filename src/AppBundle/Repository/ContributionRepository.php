8a3ceea305db52583cf69105eaaf122a87d1fc31
Adrien Pétremann <petremann.adrien@gmail.com>
2015-09-07 17:34:26 +0200

    rhum create contribution entity

---
+++ src/AppBundle/Repository/ContributionRepository.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace AppBundle\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+/**
+ * @author    Adrien Pétremann <adrien.petremann@akeneo.com>
+ * @copyright 2015 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class ContributionRepository extends EntityRepository
+{
+}
