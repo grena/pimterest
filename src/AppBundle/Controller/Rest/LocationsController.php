87cada07e1d58c8fa572e1ff1130d77b08ded016
Adrien Pétremann <petremann.adrien@gmail.com>
2015-09-08 15:48:15 +0200

    buliflak add instagram contribution on the map

--- src/AppBundle/Controller/Rest/LocationsController.php
+++ src/AppBundle/Controller/Rest/LocationsController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller\Rest;
 
+use AppBundle\Entity\Contribution;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
@@ -9,15 +10,34 @@ class LocationsController extends Controller
 {
     public function listAction()
     {
-        $locations = [
-            ['lat' => -8.05, 'lng' => -34.9],
-            ['lat' => -8.05, 'lng' => -20],
-            ['lat' => -5, 'lng' => -34],
-            ['lat' => -6, 'lng' => -34],
-            ['lat' => -7, 'lng' => -34],
-            ['lat' => -8, 'lng' => -34],
-        ];
+        $contributations = $this->getContribRepo()->findAll();
+        $locations = [];
+
+        /**
+         * @var Contribution $contrib
+         */
+        foreach($contributations as $contrib) {
+            if ($contrib->getLatitude()) {
+                $locations[] = [
+                    'lat' => (float) $contrib->getLatitude(),
+                    'lng' => (float) $contrib->getLongitude()
+                ];
+            }
+        }
+//        $locations = [
+//            ['lat' => -8.05, 'lng' => -34.9],
+//            ['lat' => -8.05, 'lng' => -20],
+//            ['lat' => -5, 'lng' => -34],
+//            ['lat' => -6, 'lng' => -34],
+//            ['lat' => -7, 'lng' => -34],
+//            ['lat' => -8, 'lng' => -34],
+//        ];
 
         return new JsonResponse($locations);
+    }
+
+    public function getContribRepo()
+    {
+        return $this->container->get('pimterest.repository.contribution');
     }
 }
