2d7fe0f8cad2d2a99af8ca3b7080a20cb4f9d8c0
Adrien Pétremann <petremann.adrien@gmail.com>
2015-09-10 16:44:39 +0200

    gulden drake : styling customers

--- src/AppBundle/Resources/public/js/templates/info-window.js
+++ src/AppBundle/Resources/public/js/templates/info-window.js
@@ -1,5 +1,5 @@
 var info_window_template = '<div class="iw-panel <%= iwClass %>">' +
-    '<div class="iw-img text-right">' +
+    '<div class="iw-img text-center">' +
     '<a href="<%= mediaUrl %>" onclick=\'CB_Open("href=<%= mediaUrl %>");return false\' rel="clearbox">' +
     '<img src="<%= mediaUrl %>" class="img-responsive img-infowindow"/>' +
     '</a>' +
